a444e6210ecbf3791dbd731f668891cce1693e6e
skipps1 <valik200511@gmail.com>
2026-06-12 13:15:44 +0200

    Added tests for Bad statuses

--- src/test/java/com/skipps/finance/controller/AuthControllerIntegrationTest.java
+++ src/test/java/com/skipps/finance/controller/AuthControllerIntegrationTest.java
@@ -55,6 +55,69 @@ public class AuthControllerIntegrationTest
     }
 
     @Test
+    void registerWithExistingUsernameReturnsConflict() throws Exception
+    {
+        String requestBody = """
+            {
+                "username": "skipps",
+                "email": "skipps@example.com",
+                "password": "password"
+            }
+        """;
+
+        mockMvc.perform(post("/api/auth/register")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(requestBody))
+            .andExpect(status().isCreated());
+
+        requestBody = """
+            {
+                "username": "skipps",
+                "email": "other@example.com",
+                "password": "password"
+            }
+        """;
+
+        mockMvc.perform(post("/api/auth/register")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(requestBody))
+            .andExpect(status().isConflict())
+            .andExpect(jsonPath("$.message").value("Username is already taken"));
+    }
+
+
+    @Test
+    void registerWithExistingEmailReturnsConflict() throws Exception
+    {
+        String requestBody = """
+            {
+                "username": "skipps",
+                "email": "skipps@example.com",
+                "password": "password"
+            }
+        """;
+
+        mockMvc.perform(post("/api/auth/register")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(requestBody))
+            .andExpect(status().isCreated());
+
+        requestBody = """
+            {
+                "username": "other",
+                "email": "skipps@example.com",
+                "password": "password"
+            }
+        """;
+
+        mockMvc.perform(post("/api/auth/register")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(requestBody))
+            .andExpect(status().isConflict())
+            .andExpect(jsonPath("$.message").value("Email is already taken"));
+    }
+
+    @Test
     void loginUserReturnsJwtAndUserDTO() throws Exception
     {
         String requestBody = """
@@ -86,5 +149,67 @@ public class AuthControllerIntegrationTest
             .andExpect(jsonPath("$.response.username").value("skipps"))
             .andExpect(jsonPath("$.response.email").value("skipps@example.com"));
 
+    }
+
+    @Test
+    void loginWithWrongPasswordReturnsUnauthorized() throws Exception
+    {
+        String requestBody = """
+            {
+                "username": "skipps",
+                "email": "skipps@example.com",
+                "password": "password"
+            }
+            """;
+
+        mockMvc.perform(post("/api/auth/register")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(requestBody))
+            .andExpect(status().isCreated());
+
+
+        requestBody = """
+            {
+                "username": "skipps",
+                "password": "pass"
+            }
+            """;
+
+        mockMvc.perform(post("/api/auth/login")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(requestBody))
+            .andExpect(status().isUnauthorized())
+            .andExpect(jsonPath("$.message").value("Wrong username or password"));
+    }
+
+    @Test
+    void loginWithWrongUsernameReturnsUnauthorized() throws Exception
+    {
+        String requestBody = """
+            {
+                "username": "skipps",
+                "email": "skipps@example.com",
+                "password": "password"
+            }
+            """;
+
+        mockMvc.perform(post("/api/auth/register")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(requestBody))
+            .andExpect(status().isCreated());
+
+
+        requestBody = """
+            {
+                "username": "skipps1",
+                "password": "password"
+            }
+            """;
+
+        mockMvc.perform(post("/api/auth/login")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(requestBody))
+            .andExpect(status().isUnauthorized())
+            .andExpect(jsonPath("$.message").value("Wrong username or password"));
     }
 }
